648bd61399feb938fa4cc4a6fc6464afb44ed3c4
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-13 01:45:21 +0300

    redid api4

--- api4.php
+++ api4.php
@@ -1,7 +1,8 @@
 <?php
-    date_default_timezone_set('Asia/Beirut');
-    $date_today = time();
-    $christmas = strtotime('December 25');
-    $days = ceil(($christmas - $date_today)/(60*60*24));
+    $time_input = strtotime($_GET['date']); 
+    $date_input = getDate($time_input);
+    $date = strtotime($date_input['mday'] . " " . $date_input['month'] . " ". $date_input['year']);
+    $christmas = strtotime('December 25 2022');
+    $days = ceil(($christmas - $date)/(60*60*24));
     echo json_encode($days);
 ?>
